386589b6434d6bdd17eac46bb21d6b1717e56ff6
Jon Mann <jon.mann@mavenclinic.com>
2024-10-18 16:14:43 -0400

    added more content and aligned story

--- apps/print_newspaper.py
+++ apps/print_newspaper.py
@@ -1,9 +1,12 @@
 import subprocess
 
+# Usage: Printer operator should run this program and provide the
+# name of each section to print by 2am for punctual delivery.
+
 def print_file(file_path):
     try:
         # Use the 'lp' command to print the file
-        subprocess.run(['lp', file_path], check=True)
+        subprocess.run(['lp', "readyForPressing/".format(file_path)], check=True)
         print(f"Printing {file_path}...")
     except subprocess.CalledProcessError as e:
         print(f"Failed to print the file: {e}")
@@ -12,5 +15,5 @@ def print_file(file_path):
 
 
 
-file_to_print = input("Enter the filename (with full path) of the article")
+file_to_print = input("Enter the filename of the section")
 print_file(file_to_print)
